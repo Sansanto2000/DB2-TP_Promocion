f8f1eaed92fe03dc8b946f426e597cf69cd50fa3
Santiago Ponte <santiagoandresponteahon@hotmail.com>
2022-08-09 19:02:58 -0300

    sada

--- promoTP/src/main/java/com/mitocode/controller/TestController.java
+++ promoTP/src/main/java/com/mitocode/controller/TestController.java
@@ -38,12 +38,21 @@ public class TestController {
 		Date date1 = new GregorianCalendar(2000, Calendar.FEBRUARY, 11).getTime();
 		Date date2 = new GregorianCalendar(2020, Calendar.FEBRUARY, 20).getTime();
 		System.out.println(accidentService.accidentsBetweenTwoDates(date1, date2));
-		return startDate;
+		return startDate; //falta agregar paginado y demas
 	}
+
 	//(postgre) determinar las condiciones más comunes en los accidentes (hora del día, condiciones climáticas, etc)
 	//(mongodb) dado un punto geográfico y un radio (expresado en kilómetros) devolver todos los accidentes ocurridos dentro del radio.
 	//(mongodb) obtener la distancia promedio desde el inicio al fin del accidente
 	//(mongodb) devolver los 5 puntos más peligrosos (definiendo un determinado radio y utilizando los datos de los accidentes registrados).
 	//(mongodb) devolver la distancia promedio que existe entre cada accidente y los 10 más cercanos.
+
 	//(postgre) devolver el nombre de las 5 calles con más accidentes.
+	@ResponseStatus(HttpStatus.OK)
+	@GetMapping("/query7")
+	public String fiveStreetsWithMoreAccidents() {
+		//Comprobaciones de formato y tipo de date1 y date2
+		System.out.println(accidentService.accidentsBetweenTwoDates());
+		return "Query 7";
+	}
 }
--- promoTP/src/main/java/com/mitocode/repo/postgre/IPostgreAccidentRepo.java
+++ promoTP/src/main/java/com/mitocode/repo/postgre/IPostgreAccidentRepo.java
@@ -12,4 +12,8 @@ import java.util.List;
 public interface IPostgreAccidentRepo extends JpaRepository<Accident, String>{
     @Query(value = "select * from accident a where (a.start_Time BETWEEN ?1 AND ?2)", nativeQuery = true)
     List<Accident> accidentsBetweenTwoDates(Date startDate, Date endDate);
+
+    @Query(value = "select street, count(street) as total from accidents group by street order by total desc limit 5", nativeQuery = true)
+    @Query(value = "select a.street from accident a group by a.Street order by count(a.Street) desc", nativeQuery = true)
+    List<Accident> accidentsBetweenTwoDates(Date startDate, Date endDate);
 }
